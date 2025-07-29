e47e11f1118415dac077794d2c625bc0755620f4
Sembatya2020 <ivansmbt@gmail.com>
2025-07-29 14:47:22 +0300

    ready

--- middleware.ts
+++ middleware.ts
@@ -7,4 +7,4 @@ export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
- // This middleware applies to all routes except API routes, static files, and PNG images.
+ // This middleware applies to all routes except API routes, static files, and PNG image.
